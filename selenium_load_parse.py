68e0b4fa43e7f6696b5e2170d808a146c0be3631
Lydia Gorham <gorhamlydia@gmail.com>
2016-08-10 17:17:05 -0700

    Running up against potential rate limit

--- selenium_load_parse.py
+++ selenium_load_parse.py
@@ -5,26 +5,29 @@ from bs4 import BeautifulSoup
 import urllib
 import sys
 import json
+import datetime
 
 reload(sys)  # Reload does the trick!
 sys.setdefaultencoding('UTF8')
 
 
-def beatiful_soup_parse(webpage):
-    """Takes in a url and parses the html webpage"""
+def beatiful_soup_parse(html):
+    """
+    Takes in a html from driver and parses for each individual tweet's information,
+    and then writes the information to a pipe delimited file for later seeding
+    """
     
-    soup = BeautifulSoup(webpage, 'html.parser')
+    soup = BeautifulSoup(html, 'html.parser')
 
     # parent_tweets = soup.findAll("div", {"class" : "content"})
     parent_tweets = soup.findAll("div", {"class" : "tweet js-stream-tweet js-actionable-tweet js-profile-popup-actionable original-tweet js-original-tweet "})
 
 
     #While it would be more concise to seed directly into my database in this step
-    #instead of parsing, saving as json and then iterating over the json while seeding
     #given the time constraints of the project, the cost of having to repeat the download/parse
     #process if I needed to drop my tables at any point was too high to make it a worthwhile tradeoff
 
-    data_file = open("data_file.txt", "w")
+    data_file = open("data_file.txt", "a")
 
     for tweet in parent_tweets:
         tweet_id = tweet['data-tweet-id']
@@ -37,17 +40,28 @@ def beatiful_soup_parse(webpage):
         handle = handles[0].text
         content = tweet_contents[0].text
         timestamp = timestamps[0]['title']
+
+        #not all twitter users provide geotags or profile location information
         if profile_location:
             profile_location = profile_location[0]['title']
+        else:
+            profile_location = ""
         if place_id:
             place_id = place_id[0]['data-place-id']
+        else:
+            place_id = ""
 
-        data_file.write("{} | {} | {} | {} | {} | {}\n".format(handle, tweet_id, content, timestamp, profile_location, place_id))
+        dataline = "|".join([handle, tweet_id, content, timestamp, profile_location, place_id])
+        data_file.write("{}\n".format(dataline))
 
 
     data_file.close()
 
-    return timestamp
+    final_date = timestamp.split(" ")
+    final_date = '-'.join(final_date[-3:])
+    last_date_recorded = datetime.datetime.strptime(final_date, "%d-%b-%Y").strftime("%Y-%m-%d")
+
+    return last_date_recorded
 
 
 
@@ -60,28 +74,31 @@ def load_page_and_parse():
     driver = webdriver.Firefox()
 
     #advanced search url - looking for tweets containing Trump OR Clinton
-    driver.get("https://twitter.com/search?q=Trump%20OR%20Clinton%20lang%3Aen&src=typd")
+    # driver.get("https://twitter.com/search?q=Trump%20OR%20Clinton%20lang%3Aen&src=typd")
     # driver.get("https://twitter.com/search?q=Trump%20OR%20Clinton%20lang%3Aen%20since%3A2016-08-08%20until%3A2016-08-09&src=typd&lang=en")
 
 
-    since = 2016-01-01
-    # until = 
+    since_date = "2016-01-01"
+    stop_date = "2016-08-02"
+    
+    while since_date == "2016-01-01":
 
-    while since == 2016-01-01:
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(4)
-        times_to_scroll -= 1
-
-    html = driver.page_source
-    last_date = beatiful_soup_parse(html)
-
+        driver.get("https://twitter.com/search?f=tweets&q=Trump%20OR%20Clinton%20until:{}&src=typd".format(str(stop_date)))
+        # driver.get("https://twitter.com/search?f=tweets&vertical=default&q=Trump until:{}&lang=eng&src=typd".format(str(stop_date)))
+        scroll_until = 100
+        while scroll_until:
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            scroll_until -= 1
+            time.sleep(2)
+            print scroll_until
+        html = driver.page_source
+        stop_date = beatiful_soup_parse(html)
+        print stop_date
 
     #close the browser instance
     driver.quit()
 
-    print last_date
-
-
+    return stop_date
 
 if __name__ == '__main__':
 
